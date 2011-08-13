8a02a8cdead62c27726bed249a81788e3863af9b
vonbecmann <vonbecmann@gmail.com>
2011-08-13 16:25:25 -0300

    rename shell

--- start-console-builder.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-HOME_DIRECTORY=$(dirname $(readlink -nf $0))
-BASE_DIRECTORY=$(dirname ${HOME_DIRECTORY})
-
-guile -L ${BASE_DIRECTORY} -l ${HOME_DIRECTORY}/console-builder.scm -e main -- --home=${HOME_DIRECTORY} 
-
-
