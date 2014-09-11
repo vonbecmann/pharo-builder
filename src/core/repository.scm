7ad12a3d6337112f39a49bf1bd8ae39796f93f4e
vonbecmann <vonbecmann@experienced.experienced>
2014-09-11 17:56:35 -0300

    change format style

--- src/core/repository.scm
+++ src/core/repository.scm
@@ -24,16 +24,13 @@
 	    directory-name
 	    set-directory-name
 	    artifact-ref
-	    artifacts-list
-	    )
-  )
+	    artifacts-list))
 
 (define-record-type <repository>
   (make-repository artifacts directory)
   repository?
   (artifacts artifacts)
-  (directory directory set-directory!)
-  )
+  (directory directory set-directory!))
 
 (set-record-type-printer! <repository>
 			  (lambda (self port)
@@ -43,61 +40,47 @@
 				    (artifacts-list self))))
 
 (define (hash-to-pair-list table)
-  (hash-map->list cons table)
-  )
+  (hash-map->list cons table))
 
 (define (artifacts-list self)
-  (hash-to-pair-list (artifacts self))
-  )
+  (hash-to-pair-list (artifacts self)))
 
 (define (directory-name self)
-  (directory self)
-  )
+  (directory self))
 
 (define (set-directory-name self directory)
-  (set-directory! self directory)
-  )
+  (set-directory! self directory))
 
 (define (add-artifact self artifact)
   (hashq-set! (artifacts self) (artifact-name artifact) artifact)
-  artifact
-  )
+  artifact)
 
 (define (add-new-artifact self name download-url source)
-  (add-artifact self (make-artifact-for name download-url source self))
-  )
+  (add-artifact self (make-artifact-for name download-url source self)))
 
 (define (add-new-vm self name download-url path-to-executable)
-  (add-artifact self (make-vm-for name download-url path-to-executable self))
-  )
+  (add-artifact self (make-vm-for name download-url path-to-executable self)))
 
 (define (add-new-source self name download-url)
-  (add-artifact self (make-source name download-url self))
-  )
+  (add-artifact self (make-source name download-url self)))
 
 (define (hash-to-artifact-list table)
-  (hash-map->list (lambda (key value) value) table)
-  )
+  (hash-map->list (lambda (key value) value) table))
 
 (define (download-all self)
-  (map download (hash-to-artifact-list (artifacts self)))
-  )
+  (map download (hash-to-artifact-list (artifacts self))))
 
 (define (build-repo self)
   (mk-directory (directory-name self))
-  (download-all self)
-  )
+  (download-all self))
 
 (define (remove self)
-  (rm-directory (directory-name self))
-  )
+  (rm-directory (directory-name self)))
 
 (define (new-repository-for directory)
-  (make-repository (make-hash-table) directory)
-  )
+  (make-repository (make-hash-table) directory))
 
 (define (artifact-ref self artifact-name)
-  (hashq-ref (artifacts self) artifact-name)
-  )
+  (hashq-ref (artifacts self) artifact-name))
 
 ;;; repository.scm ends here
